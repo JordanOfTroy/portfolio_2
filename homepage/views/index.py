35676a501b80b6a0078803781f78a1e809497bc1
Jordan Smithson <jordantroysmithson@gmail.com>
2019-05-27 19:02:26 -0600

    contact section finished and printing input info

--- homepage/views/index.py
+++ homepage/views/index.py
@@ -1,25 +1,22 @@
 from django.conf import settings
 from django_mako_plus import view_function, jscontext
 from datetime import datetime, timezone
-from django import forms
-from homepage import models as cmod
 
-
-class NameForm(forms.Form):
-    first_name_yo = forms.CharField(label='first name', max_length=30)
-    last_name = forms.CharField(label='last name', max_length=30)
 
 @view_function
 def process_request(request):
 
-    form = NameForm()
+  
 
     if request.method == 'POST':
-        if form.is_valid():
-            u = cmod.Person()
-            u.first_name_yo = form.cleaned_data.get('first_name')
-            u.last_name = form.cleaned_data.get('last_name')
-    
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        message = request.POST.get('message', '')
+
+        print('-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
+        print(name)
+        print(email)
+        print(message)
 
     utc_time = datetime.utcnow()
     context = {
@@ -27,6 +24,6 @@ def process_request(request):
         'utc_time': utc_time,
         # sent to index.html and index.js:
         jscontext('utc_epoch'): utc_time.timestamp(),
-        'form': form
+        
     }
     return request.dmp.render('index.html', context)
